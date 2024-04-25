53aa860c8defc26f3dc68e5b1c1138465254ba0a
Liam <liamsuu14974@gmail.com>
2024-04-25 15:48:53 +0100

    Complete functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+export default function toggleDropdown(...elements) {
+  elements.forEach((hoverElement) => {
+    const currentElement = hoverElement;
+    currentElement.className = "dropDownButton";
+  });
+  document.querySelectorAll(".dropDownButton").forEach((hoverElement) => {
+    const elementButton = hoverElement;
+    elementButton.nextElementSibling.style.display = "none";
+    elementButton.nextElementSibling.style.position = "absolute";
+    elementButton.nextElementSibling.style.zIndex = 1;
+    elementButton.parentElement.style.display = "inline-block";
+    elementButton.parentElement.style.position = "relative";
 
+    elementButton.onmouseover = () => {
+      elementButton.nextElementSibling.style.display = "block";
+    };
+
+    elementButton.onmouseout = () => {
+      elementButton.nextElementSibling.style.display = "none";
+    };
+  });
+}
